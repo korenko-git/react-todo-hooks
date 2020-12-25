33d24db1ea206192c61d95abb0934d3c6db33b9d
Dmitry Korenko <depiict@gmail.com>
2020-12-25 16:40:54 +0200

    test: add mock theme

---
+++ src/themes/mockTheme.js
@@ -0,0 +1,5 @@
+import { ThemeConsumer } from 'styled-components';
+import lightTheme from './lightTheme';
+
+// eslint-disable-next-line no-underscore-dangle
+ThemeConsumer._currentValue = lightTheme;
