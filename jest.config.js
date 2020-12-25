26b826842f6502ea5a8ac647e1fa5084933827a2
Dmitry Korenko <depiict@gmail.com>
2020-12-25 20:49:41 +0200

    test: add svgr mock

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,8 @@ module.exports = {
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '.*\\.(css|scss|sass)$': '<rootDir>/__mocks__/style.js',
-    '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
+    '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/image.js',
   },
   testMatch: [
