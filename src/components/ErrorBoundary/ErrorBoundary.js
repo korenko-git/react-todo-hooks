3939ef4d4ab8bb8c1dc62361350e192b45338978
Dmitry Korenko <depiict@gmail.com>
2020-12-25 13:11:48 +0200

    fix(ErrorBoundary): import style

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import { Wrapper } from './ErrorBoundary.css';
+import { Wrapper } from './ErrorBoundary.css.js';
 
 export default class ErrorBoundary extends Component {
   constructor(props) {
